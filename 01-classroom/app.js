fe257950cec17869da499935967379e9ef2f08fd
wellpinho <wellpinho@outlook.com>
2023-03-19 14:11:56 -0300

    refactoring

--- 01-classroom/app.js
+++ 01-classroom/app.js
@@ -21,98 +21,56 @@ Utilize os espaços entre os comentários abaixo para executar os códigos dos
 exercícios. Inclusive o alert mencionado acima.
 */
 
-// const printMessage = () => alert('JS is Awesome!');
-
-
+// alert('JS is Awesome!');
 
 /*
 04 - Comente o alert que você escreveu acima, utilizando o comentário de uma linha.
 */
 
-(() => {
-    /*
-    05 - Declare uma const "myBirthYear", que recebe o ano do seu nascimento, com 4  
-    dígitos. Ex.: 1930.
-    */
-    const age = document.querySelector('.age');
-    
-    const myBirthYear = (age) => {
-        const yearsOld = 2023 - age;
-        return `My year of birth: ${yearsOld}`;
-    }
-    
-    age.innerHTML = myBirthYear(44)
-})();
+/*
+05 - Declare uma const "myBirthYear", que recebe o ano do seu nascimento, com 4  
+dígitos. Ex.: 1930.
+*/
+const myBirthYear = 44;
 
-(() => {
-    /*
-    06 - Declare uma const "currentYear", que recebe o ano atual, com 4 dígitos.
-    */
-    const year = document.querySelector('.currentYear');
-    
-    const currentYear = () => {
-        const current = new Date().getFullYear()
-        return `Current year: ${current}`;
-    }
-    
-    year.innerHTML = currentYear();
+/*
+06 - Declare uma const "currentYear", que recebe o ano atual, com 4 dígitos.
+*/
+const currentYear = 2023;
 
-})();
+/*
+07 - Exiba as const "myBirthYear" e "currentYear", lado a lado, no console do browser.
+*/
 
-(() => {
-    /*
-    07 - Exiba as const "myBirthYear" e "currentYear", lado a lado, no console do browser.
-    */
-    const myBirthYear = (age) => {
-        const yearsOld = 2023 - age;
-        return `My year of birth: ${yearsOld}`;
-    }
+console.log(myBirthYear, currentYear)
 
-    const currentYear = () => {
-        const current = new Date().getFullYear()
-        return `Current year: ${current}`;
-    }
-    
-    console.log('exercise 7: ', myBirthYear(43), currentYear())
+/*
+08 - Experimente.
+- Declare uma const "myAge", que recebe o ano atual subtraído pelo ano do seu  
+nascimento. Mas sem digitar os números manualmente.
+Você pode utilizar o sinal de menos (-) entre os dois valores e verificar o que  
+acontece. 
+- Exiba a "myAge" no console.
+*/
+const myAge = currentYear - myBirthYear;
 
-})();
+console.log(myAge);
 
-(() => {
-    /*
-    08 - Experimente.
-    - Declare uma const "myAge", que recebe o ano atual subtraído pelo ano do seu  
-    nascimento. Mas sem digitar os números manualmente.
-    Você pode utilizar o sinal de menos (-) entre os dois valores e verificar o que  
-    acontece. 
-    - Exiba a "myAge" no console.
-    */
-    const actualYear = new Date().getFullYear();
-    const age = 44;
-    
-    const myAge = (year, age) => {
-        return year - age;
-    }
-    
-    console.log(myAge(actualYear, age))
-})();
+/*
+09 - Antes do console.log abaixo, declare uma const "ten", que recebe o número  
+10 e:
+- Descomente o console.log;
+- Substitua a palavra "valor1" pelo ano atual somado à 10;
+- Substitua a palavra "valor2" pela sua idade somada à 10;
+- Os números não devem ser digitados manualmente;
+- Observe o resultado no console do browser.
+Não se assuste com as crases e os cifrões. Conversaremos mais sobre operações  
+matemáticas e strings (textos) em breve =)
+*/
 
-(() => {
-    /*
-    09 - Antes do console.log abaixo, declare uma const "ten", que recebe o número  
-    10 e:
-    - Descomente o console.log;
-    - Substitua a palavra "valor1" pelo ano atual somado à 10;
-    - Substitua a palavra "valor2" pela sua idade somada à 10;
-    - Os números não devem ser digitados manualmente;
-    - Observe o resultado no console do browser.
-    Não se assuste com as crases e os cifrões. Conversaremos mais sobre operações  
-    matemáticas e strings (textos) em breve =)
-    */
-    
-    const ten = 10;
-    
-    console.log(`Em ${2023 + ten}, estarei com ${44 + ten} anos.`)
-})();
+const ten = 10;
+
+console.log(`Em ${currentYear + ten}, estarei com ${myBirthYear + ten} anos.`)
 
 /*
 10 - Abra o site abaixo no browser e faça o seguinte:
