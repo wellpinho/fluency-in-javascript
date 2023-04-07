b4501109d8ad2aea53fa693a84142418eb80fd77
wellpinho <wellpinho@outlook.com>
2023-04-07 17:38:01 -0300

    classroom 12 query selector byId by className and tagname

---
+++ 12-classroom-DOM/app.js
@@ -0,0 +1,20 @@
+// const paragraph = document.querySelector('p'); // pega apenas o primeiro p
+// console.log(paragraph)
+
+// const h1 = document.querySelectorAll('body > h1');
+// console.log(h1)
+
+// const paragraphs = document.querySelectorAll('p')
+// paragraphs.forEach(paragraph => console.log(paragraph))
+
+// get by id
+const h1 = document.getElementById('h1')
+console.log(h1)
+
+// get element by class name
+const errors = document.getElementsByClassName('error')
+console.log(errors)
+
+// get by tag name
+const p = document.getElementsByTagName('p')
+console.log(p)
